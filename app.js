a723524a1fc9fdcc6c4876b7c0095c869c183499
sergiocosta-lams <sergiohenriquerc@gmail.com>
2026-07-28 09:49:46 -0300

    Refactor data fetching and rendering logic
    Updated the fetchData function to use Number for parsing prices and simplified the render function to display all data without filtering.

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-window.onload = function(){
+window.onload = async function(){
 
 const sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRQPgvZ0cXCg26_S-P3DzugU3e8AbdfDeEh-Q6FQDV66skoz_reYeTcrWuYYMUh2kFQmllE5ogovu2O/pub?output=csv";
 
 
-async function fetchData() {
+async function fetchData(){
 
     const res = await fetch(sheetURL);
     const text = await res.text();
@@ -19,8 +19,8 @@ async function fetchData() {
             bandeira: cols[1],
             endereco: cols[2],
             maps: cols[3],
-            gasolina: parseFloat(cols[4]),
-            etanol: parseFloat(cols[5]),
+            gasolina: Number(cols[4]),
+            etanol: Number(cols[5]),
             horario: cols[6]
         };
 
@@ -31,55 +31,39 @@ async function fetchData() {
 async function render(){
 
     const list = document.getElementById("list");
+
     list.innerHTML = "Carregando...";
 
+
     const data = await fetchData();
-
-    const search = document.getElementById("search").value.toLowerCase();
-    const filter = document.getElementById("filter").value;
-
-
-    let filtered = data.filter(item =>
-        item.nome?.toLowerCase().includes(search)
-    );
-
-
-    let minPrice = Math.min(...filtered.map(item => item.gasolina));
 
 
     list.innerHTML = "";
 
 
-    filtered.forEach(item => {
+    data.forEach(item => {
 
         const div = document.createElement("div");
 
         div.className = "card";
 
 
-        if(item.gasolina === minPrice){
-            div.classList.add("cheapest");
-        }
-
-
         div.innerHTML = `
-            <h3>${item.nome}</h3>
 
-            <p>⛽ Gasolina:
-            <strong>R$ ${item.gasolina.toFixed(2).replace(".", ",")}</strong>
-            </p>
+        <h3>${item.nome}</h3>
 
-            <p>🌱 Etanol:
-            <strong>R$ ${item.etanol.toFixed(2).replace(".", ",")}</strong>
-            </p>
+        <p>⛽ Gasolina: R$ ${item.gasolina.toFixed(2)}</p>
 
-            <p>⏰ ${item.horario}</p>
+        <p>🌱 Etanol: R$ ${item.etanol.toFixed(2)}</p>
 
-            <p>📍 ${item.endereco}</p>
+        <p>⏰ ${item.horario}</p>
 
-            <a href="${item.maps}" target="_blank" class="btn-maps">
-                📍 Como chegar
-            </a>
+        <p>📍 ${item.endereco}</p>
+
+        <a href="${item.maps}" target="_blank">
+        📍 Como chegar
+        </a>
+
         `;
 
 
@@ -90,15 +74,7 @@ async function render(){
 }
 
 
-document.getElementById("search")
-.addEventListener("input", render);
-
-
-document.getElementById("filter")
-.addEventListener("change", render);
-
-
 render();
 
 
-}
+};
